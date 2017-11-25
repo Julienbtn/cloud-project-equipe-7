435e987aa4490b93021a9fc5ca99a1ac0faeda99
Joymangul Jensen Selwyn <jensen.joymangul@etu.univ-lyon1.fr>
2017-11-25 20:54:59 +0100

    :sparkles: All the requested APIs are implemented and CI is working (#16)
    * :ambulance: Fix position type for tests
    
    * :ambulance: Change mongoDB IP
    
    * :green_heart: Add mongodb services to travis
    
    * :sparkles: CI fully implemented and installation script is working (#15)
    
    * beginning of the presentation
    
    * Add dockerfile
    
    * Add dockerfile
    
    * change travis yml
    
    * Add different profile for project
    
    * Update pom.xml
    
    * Update Dockerfile to build the correct profile
    
    * Add docker-compose
    
    * Remove unused environment in docker compose
    
    * beginning of the presentation
    
    * Add dockerfile
    
    * Add dockerfile
    
    * change travis yml
    
    * Add different profile for project
    
    * Update pom.xml
    
    * Update Dockerfile to build the correct profile
    
    * Add docker-compose
    
    * Remove unused environment in docker compose
    
    * Update database config for travis
    
    * :ambulance:Can pass the latest client tests
    
    * beginning of the presentation
    
    * Add install script
    
    * Add docker-compose
    
    * Remove unused environment in docker compose
    
    * Add dockerfile
    
    * Add different profile for project
    
    * Add docker-compose
    
    * Remove unused environment in docker compose
    
    * Update database config for travis
    
    * Install script now deletes previous image and container
    
    * Detach docker in the install script
    
    * Wait before testing application in script
    
    * Improve display for installation script
    
    * Improve display for installation script
    
    * Update docker image only on master branch
    
    * Fix typo
    
    * Update URL in deployment script

---
+++ deploy/install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Install docker
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Install docker compose
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
+
+# Download the docker-compose.yml
+sudo rm -f docker-compose.yml
+wget https://raw.githubusercontent.com/PolytechLyon/cloud-project-equipe-7/master/deploy/docker-compose.yml
+
+# Delete previous containers and images
+docker rm -f mongodb
+docker rm -f cloud-java
+
+docker rmi julienbtn/cloud-java
+
+docker-compose up -d
+
+echo Waiting for the application to start
+sleep 15  # Waits 15 seconds.
+
+HTTP_STATUS="$(curl -IL --silent http://localhost:80/user | grep HTTP )";
+echo "${HTTP_STATUS}"
+
+# Remove docker-compose.yml file
+sudo rm -f docker-compose.yml
