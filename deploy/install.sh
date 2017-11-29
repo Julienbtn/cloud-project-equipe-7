620bce1b3bb122d6f0e020f6fd6d16ca5156f190
Matthieu MOISSON <matthieu.moisson38@gmail.com>
2017-11-29 14:14:53 +0100

    :speech_balloon: update install.sh

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Install docker
-
+echo "##############################################################################################"
+echo "####################################### Install Docker #######################################"
+echo "##############################################################################################"
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
 sudo apt-get install \
@@ -25,15 +27,24 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 # Install docker compose
+echo "##############################################################################################"
+echo "################################### Install docker compose ###################################"
+echo "##############################################################################################"
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -v
 
 # Download the docker-compose.yml
+echo "##############################################################################################"
+echo "############################## Download the docker-compose.yml ###############################"
+echo "##############################################################################################"
 sudo rm -f docker-compose.yml
 wget https://raw.githubusercontent.com/PolytechLyon/cloud-project-equipe-7/master/deploy/docker-compose.yml
 
 # Delete previous containers and images
+echo "##############################################################################################"
+echo "########################### Delete previous containers and images ############################"
+echo "##############################################################################################"
 docker rm -f mongodb
 docker rm -f cloud-java
 
@@ -48,4 +59,7 @@ HTTP_STATUS="$(curl -IL --silent http://localhost:80/user | grep HTTP )";
 echo "${HTTP_STATUS}"
 
 # Remove docker-compose.yml file
+echo "##############################################################################################"
+echo "############################### Remove docker-compose.yml file ###############################"
+echo "##############################################################################################"
 sudo rm -f docker-compose.yml
